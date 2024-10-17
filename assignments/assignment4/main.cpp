7585abeb0cc2ed6e400ede7cda9b3b4ca0e068ec
Layne Chant <layne.chant@gmail.com>
2024-10-17 19:32:36 -0400

    Finished implementing the camera

--- assignments/assignment4/main.cpp
+++ assignments/assignment4/main.cpp
@@ -12,10 +12,28 @@
 #include <Shader File/shader.h>
 #include <Shader File/texture2D.h>
 
+void processInput(GLFWwindow  *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+
+glm::vec3 cameraPos		= glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront	= glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp		= glm::vec3(0.0f, 1.0f,  0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;
+float pitch = 0.0f; 
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
+float deltaTime = 0.0f; 
+float lastFrame = 0.0f;
 
 int main() {
 	printf("Initializing...");
@@ -33,6 +51,12 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	glEnable(GL_DEPTH_TEST);
 
 	float vertices[] = {
 		//X      Y    Z    texture coords
@@ -98,6 +122,8 @@ int main() {
 		glm::vec3(1.5f,  0.2f, -1.5f),
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
+
+
 	
 	//Initialization goes here!
 	unsigned int VAO, VBO, EBO;
@@ -122,25 +148,29 @@ int main() {
 	glEnableVertexAttribArray(1);
 
 
+
 	shaderFile::Texture2D brick("assets/wall.jpg", GL_LINEAR, GL_REPEAT);
 	unsigned int brickTexture = brick.GetID();
 
 	// creates the shader object
 	shaderFile::Shader brickShader("assets/vertexShader.vert", "assets/fragmentShader.frag");
 	
-
-
 	// defining the projection matrix
 	glm::mat4 projection;
-	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+	projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
 
 	int timeLocation = glGetUniformLocation(brickShader.ID, "uTime");
 
-	glEnable(GL_DEPTH_TEST);
+
 
 	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+		
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
+		processInput(window);
+		
 		//Clear framebuffer
 		glClearColor(0.0f, 0.5f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -154,17 +184,34 @@ int main() {
 		glBindTexture(GL_TEXTURE_2D, brickTexture);
 
 		// sets camera FOV and viewport
-		glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
+		//glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
+
+		//// sets camera target and direction
+		//glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+		//glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+		//glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+		//glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+
+		//glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+		//const float radius = 10.0f; 
+		//float camX = sin(glfwGetTime()) * radius; 
+		//float camZ = cos(glfwGetTime()) * radius; 
+		//
+		
+
 
 		brickShader.use();
 
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
 		// creats the transforms 
 		glm::mat4 projection = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
 
-		projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+		projection = glm::perspective(glm::radians(fov), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 		
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
 		// gets the matrix uniform locations
 		unsigned int modelLoc = glGetUniformLocation(brickShader.ID, "model");
@@ -193,4 +240,66 @@ int main() {
 		glfwPollEvents();
 	}
 	printf("Shutting down...");
+
+	
+}
+void processInput(GLFWwindow* window)
+{
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	const float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos; 
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX; 
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos; 
+
+	const float sensitivity = 0.1f; 
+	xoffset  *= sensitivity; 
+	yoffset  *= sensitivity; 
+
+	yaw   += xoffset; 
+	pitch += yoffset;
+
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw) * cos(glm::radians(pitch)));
+	direction.z = sin(glm::radians(yaw));
+	direction.y = sin(glm::radians(pitch) * cos(glm::radians(pitch)));
+	cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	fov -= (float)yoffset;
+	if (fov < 1.0f)
+		fov = 1.0f; 
+	if (fov > 45.0f)
+		fov = 45.0f;
 }
