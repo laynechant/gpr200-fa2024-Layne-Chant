208b77af954770906c436cd4fb607f0ea55717e7
User <User@MRBEAST.local>
2024-12-05 14:05:11 -0500

    Got particles back on the screen with textures!

--- assignments/assignment6/main.cpp
+++ assignments/assignment6/main.cpp
@@ -21,354 +21,200 @@
 #include <Shader File/model.h>
 #include <Shader File/particleSystem.h>
 
-using namespace std; 
+using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow  *window);
+void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
-const int MAX_PARTICLES = 750;
+const int MAX_PARTICLES = 500;
 
 shaderFile::Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX = SCREEN_WIDTH / 2.0f; 
-float lastY = SCREEN_HEIGHT / 2.0f; 
+float lastX = SCREEN_WIDTH / 2.0f;
+float lastY = SCREEN_HEIGHT / 2.0f;
 bool firstMouse = true;
 
 float deltaTime = 0.0f; // time between current frame and last frame
 float lastFrame = 0.0f;
 
+
 int main() {
-	printf("Initializing...");
-	if (!glfwInit()) {
-		printf("GLFW failed to init!");
-		return 1;
-	}
-	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
-	if (window == NULL) {
-		printf("GLFW failed to create window");
-		return 1;
-	}
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGL(glfwGetProcAddress)) {
-		printf("GLAD Failed to load GL headers");
-		return 1;
-	}
-
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init();
-
-	glEnable(GL_DEPTH_TEST);
-
-	float vertices[] = {
-	-0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-	 0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-	-0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-
-	-0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  0.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  1.0f, 1.0f,
-	-0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  0.0f, 0.0f,
-
-	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-	 0.5f,  0.5f,  0.5f, 1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f, 1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f, 1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-	-0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-	-0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-
-	-0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 1.0f
-	};
-
-	unsigned int indices[] =
-	{
-		 0, 1, 3, // first triangle
-		1, 2, 3  // second triangle
-	};
-
-	glm::vec3 cubePositions[20];
-	float cubePosRange = 10.0f; 
-	for (int i = 0; i < 20; i++)
-	{
-		cubePositions[i] = glm::vec3(ew::RandomRange(-cubePosRange, cubePosRange), ew::RandomRange(-cubePosRange, cubePosRange), ew::RandomRange(-cubePosRange, cubePosRange));
-	}
-
-	glm::vec3 cubeAngles[20];
-	for (int i = 0; i < 20; i++)
-	{
-		cubeAngles[i] = glm::vec3(glm::radians(ew::RandomRange(-0.0, 360.0f)), glm::radians(ew::RandomRange(-0.0, 360.0f)), glm::radians(ew::RandomRange(-0.0, 360.0f)));
-	}
-	
-	//Initialization goes here!
-	unsigned int VAO, VBO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	glGenBuffers(1, &EBO);
-	glGenBuffers(1, &VBO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Position (XYZ)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	 //Color RGBA
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	//glEnable(GL_TEXTURE_2D);
-
-	shaderFile::Texture2D brick("assets/wall.jpg", GL_LINEAR, GL_REPEAT);
-	unsigned int brickTexture = brick.GetID();
-
-	// creates the shader object
-	shaderFile::Shader brickShader("assets/vertexShader.vert", "assets/fragmentShader.frag");
-	shaderFile::Shader lightShader("assets/vertexShaderLight.vert", "assets/fragmentShaderLight.frag");
-
-	shaderFile::Shader ourShader("assets/modelLoading.vert", "assets/modelLoading.frag");
-
-	stbi_set_flip_vertically_on_load(true);
-
-	ShaderFile::Model backpackModel("assets/backpack.obj");
-	
-	// defining the projection matrix
-	glm::mat4 projection;
-	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-	
-	int timeLocation = glGetUniformLocation(brickShader.ID, "uTime");
-	int ambientLocation = glGetUniformLocation(brickShader.ID, "ambientStrength");
-	int diffLocation = glGetUniformLocation(brickShader.ID, "diffStrength"); 
-	int specularLocation = glGetUniformLocation(brickShader.ID, "specularStrength");
-	int shininessLocation = glGetUniformLocation(brickShader.ID, "shininess");
-
-	glm::vec3 lightPosition = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
-
-	float ambientStrength = 0.1f; 
-
-	float diffStrength = 1.0f;
-	float specularStrength = 0.5f;
-	float shininess = 512.0f;
-
-	ParticleSystem particleSystem(1000);
-
-	while (!glfwWindowShouldClose(window)) {
-		float currentFrame = static_cast<float>(glfwGetTime());
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-
-		processInput(window);
-
-		// Clear framebuffer
-		glClearColor(0.0f, 0.5f, 0.0f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// Spawn a particle every frame
-		glm::vec2 spawnPosition(0.0f, 0.0f); // Example: center of the screen
-		glm::vec2 randomVelocity(((float)rand() / RAND_MAX) * 2.0f - 1.0f, ((float)rand() / RAND_MAX) * 2.0f - 1.0f);
-		glm::vec4 randomColor(((float)rand() / RAND_MAX), ((float)rand() / RAND_MAX), ((float)rand() / RAND_MAX), 1.0f);
-		float lifetime = 2.0f;
-
-		particleSystem.SpawnParticle(spawnPosition, randomVelocity, randomColor, lifetime);
-
-		// Update and render particles
-		particleSystem.Update(deltaTime);
-		particleSystem.Render();
-
-		// update the time
-		float time = (float)glfwGetTime();
-		brickShader.use();
-
-		glUniform1f(timeLocation, time);
-		glUniform1f(ambientLocation, ambientStrength);
-		glUniform1f(diffLocation, diffStrength);
-		glUniform1f(specularLocation, specularStrength);
-		glUniform1f(shininessLocation, shininess);;
-<<<<<<< HEAD
-
-		brickShader.setVec3("lightPos", lightPosition);
-		brickShader.setVec3("lightColor", lightColor);
-
-		//glActiveTexture(GL_TEXTURE0);
-		//glBindTexture(GL_TEXTURE_2D, brickTexture);
-
-=======
-
-		brickShader.setVec3("lightPos", lightPosition);
-		brickShader.setVec3("lightColor", lightColor);
-
-		//glActiveTexture(GL_TEXTURE0);
-		//glBindTexture(GL_TEXTURE_2D, brickTexture);
-
->>>>>>> parent of 52a905a... Finished implementing Model loading
-		glm::mat4 view = camera.GetViewMatrix();
-		//brickShader.setMat4("view", view);
-
-		
-		ourShader.use();
-		// creats the transforms 
-		glm::mat4 projection = glm::mat4(1.0f);
-
-		projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-
-		
-
-		ourShader.setMat4("prjection", projection);
-		//glm::mat4 view = camera.GetViewMatrix();
-		ourShader.setMat4("view", view);
-
-		glm::mat4 model2 = glm::mat4(1.0f);
-		model2 = glm::translate(model2, glm::vec3(0.0f, 0.0f, 0.0f));
-		model2 = glm::scale(model2, glm::vec3(1.0f, 1.0f, 1.0f));
-		ourShader.setMat4("model2", model2);
-
-		backpackModel.draw(ourShader);
-
-		glm::mat4 modelProjection;
-		modelProjection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-
-		
-	
-		//// gets the matrix uniform locations
-		//unsigned int modelLoc = glGetUniformLocation(brickShader.ID, "model");
-		//unsigned int viewLoc = glGetUniformLocation(brickShader.ID, "view");
-
-		// pass the uniforms to the shaders 
-		//glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-
-		//brickShader.setMat4("projection", projection);
-
-		// DRAW CALL
-		/*glBindVertexArray(VAO);
-		for (unsigned int i = 0; i < 20; i++)
-		{
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), (cubeAngles[i] + glm::vec3(0.5f, 1.0f, 0.0f)) * deltaTime);
-			 
-			brickShader.setMat4("model", model);
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}*/
-
-		//lightShader.use();
-		//lightShader.setMat4("projection", projection);
-		//lightShader.setMat4("view", view);
-		//lightShader.setVec3("ourColor", lightColor);
-
-		//glm::mat4 model = glm::mat4(1.0f);
-		//model = glm::translate(model, lightPosition);
-		//model = glm::rotate(model, glm::radians(1.00f), (glm::vec3(0.5f, 1.0f, 0.0f) * deltaTime));
-		//model = glm::scale(model, glm::vec3(1));
-		//lightShader.setMat4("model", model);
-		//glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		//lightShader.setVec3("lightPos", lightPosition);
-		//lightShader.setVec3("lightColor", lightColor);
-
-
-		
-		
-		//glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		
-
-
-		ImGui_ImplGlfw_NewFrame();
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui::NewFrame();
-
-		ImGui::Begin("Settings");
-		ImGui::Text("Controls");
-		ImGui::DragFloat3("Light Position", &lightPosition.x, 0.1f);
-		ImGui::ColorEdit3("Light Color", &lightColor.r);
-		ImGui::SliderFloat("Ambient K", &ambientStrength, 0.0f, 1.0f);
-		ImGui::SliderFloat("Diffuse K", &diffStrength, 0.0f, 1.0f);
-		ImGui::SliderFloat("Specular K", &specularStrength, 0.0f, 1.0f);
-		ImGui::SliderFloat("Shininess", &shininess, 2.0f, 1024.0f);
-		ImGui::End();
-
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		// Drawing happens here
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	printf("Shutting down...");
+    printf("Initializing...\n");
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        printf("GLFW failed to init!\n");
+        return 1;
+    }
+
+    // Create a window
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+    if (window == NULL) {
+        printf("GLFW failed to create window\n");
+        return 1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Load OpenGL functions using glad
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        printf("GLAD Failed to load GL headers\n");
+        return 1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
+    // Enable depth testing
+    glEnable(GL_DEPTH_TEST);
+
+    // Create a shader program
+    shaderFile::Shader brickShader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+    shaderFile::Shader lightShader("assets/vertexShaderLight.vert", "assets/fragmentShaderLight.frag");
+
+    // Load the shaders for the object
+    shaderFile::Shader ourShader("assets/modelLoading.vert", "assets/modelLoading.frag");
+    shaderFile::Shader particleShader("assets/particleShader.vert", "assets/particleShader.frag");
+
+    // Make sure the model isnt upside down
+    stbi_set_flip_vertically_on_load(true);
+
+   // Load the object
+
+    ShaderFile::Model backpackModel("assets/backpack.obj");
+    shaderFile::Texture2D snowflakeTexture("assets/snowflake.png", GL_LINEAR, GL_REPEAT, true);
+    snowflakeTexture.Bind();
+   
+
+
+    ParticleSystem particleSystem(750, particleShader, glm::vec3(0.0f, 0.0f, 0.0f), snowflakeTexture.GetID());
+    // Define the projection matrix
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    // Uniform variables
+    int timeLocation = glGetUniformLocation(brickShader.ID, "uTime");
+    int ambientLocation = glGetUniformLocation(brickShader.ID, "ambientStrength");
+    int diffLocation = glGetUniformLocation(brickShader.ID, "diffStrength");
+    int specularLocation = glGetUniformLocation(brickShader.ID, "specularStrength");
+    int shininessLocation = glGetUniformLocation(brickShader.ID, "shininess");
+
+    glm::vec3 lightPosition = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+
+    float ambientStrength = 0.1f;
+
+    float diffStrength = 1.0f;
+    float specularStrength = 0.5f;
+    float shininess = 512.0f;
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen
+        glClearColor(0.0f, 0.5f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Calculate the frame time
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // Process input events
+        processInput(window);
+        // Update the particle system
+        particleSystem.update(deltaTime);
+
+        // Set up the view/projection matrices
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 model = glm::mat4(1.0f);
+
+        // Render the particles
+        particleSystem.render(view, projection);
+
+        // Render the other objects
+        ourShader.use();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+        ourShader.setMat4("model", model);
+       //backpackModel.draw(ourShader);
+
+        // Render the GUI
+        ImGui_ImplGlfw_NewFrame();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Settings");
+        ImGui::Text("Particle System");
+        //ImGui::DragFloat3("Emitter Position", &particleSystem.emitterPosition.x, 0.1f);
+        if (ImGui::Button("Emit Particle"))
+{
+            particleSystem.EmitParticle();
+        }
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean up
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+
+    return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 void processInput(GLFWwindow* window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 
-	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
-		camera.MovementSpeed = camera.BaseMovementSpeed * 2;
+    // Handle sprint (SHIFT key)
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        camera.MovementSpeed = camera.BaseMovementSpeed * 2;
+    else
+        camera.MovementSpeed = camera.BaseMovementSpeed;
 
-	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
-		camera.MovementSpeed = camera.BaseMovementSpeed;
+    // Mouse cursor locking logic: hold right mouse button to lock/unlock cursor
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS) {
+        if (glfwGetInputMode(window, GLFW_CURSOR) != GLFW_CURSOR_DISABLED) {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // Lock cursor
+        }
+    }
+    else {
+        if (glfwGetInputMode(window, GLFW_CURSOR) != GLFW_CURSOR_NORMAL) {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // Unlock cursor
+        }
+    }
 
-	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS)
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // locks the mouse
-	else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_RELEASE)
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // unlocks the mouse
-	
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		camera.ProcessKeyboard(DOWN, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-		camera.ProcessKeyboard(UP, deltaTime);
+    // Handle camera movement input (W, A, S, D, Q, E)
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        camera.ProcessKeyboard(DOWN, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        camera.ProcessKeyboard(UP, deltaTime);
 }
+
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -379,23 +225,23 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // glfw: whenever the mouse moves, this callback is called
 void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
-	float xpos = static_cast<float>(xposIn);
-	float ypos = static_cast<float>(yposIn);
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
 
-	if (firstMouse)
-	{
-		lastX = xpos; 
-		lastY = ypos;
-		firstMouse = false;
-	}
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
 
-	float xoffset = xpos - lastX; 
-	float yoffset = lastY - ypos;
-	lastX = xpos;
-	lastY = ypos;
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
 
-	if(glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
-		camera.ProcessMouseMovement(xoffset, yoffset);
+    if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED) {
+        camera.ProcessMouseMovement(xoffset, yoffset);
+    }
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
--- core/Shader File/particleSystem.h
+++ core/Shader File/particleSystem.h
@@ -1,41 +1,160 @@
-#ifndef PARTICLESYSTEM_H
-#define PARTICLESYSTEM_H
-#include <glm/glm.hpp>
+#ifndef PARTICLE_SYSTEM_H
+#define PARTICLE_SYSTEM_H
+
 #include <vector>
-#include "../ew/external/glad.h"
-#include <glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <random>
+#include <iostream>
+#include "shader.h"
 
-struct Particle {
-    glm::vec2 Position; // Position of the particle
-    glm::vec2 Velocity; // Velocity of the particle
-    glm::vec4 Color;    // RGBA color of the particle
-    float Lifetime;     // Remaining life of the particle in seconds
-
-    Particle()
-        : Position(0.0f), Velocity(0.0f), Color(1.0f), Lifetime(0.0f) {}
-};
-
-// Particle system to manage and render particles
 class ParticleSystem {
 public:
-    // Constructor
-    ParticleSystem(unsigned int maxParticles);
+    struct Particle {
+        glm::vec3 position;
+        glm::vec3 velocity = glm::vec3(1.0f);
+        glm::vec2 texCoord;
+        float lifetime = 10000.0f;
+    };
 
-    // Updates all particles in the system
-    void Update(float deltaTime);
+    std::vector<Particle> particles;
+    unsigned int maxParticles;
+    unsigned int VAO, VBO, EBO, textureID;
+    shaderFile::Shader& particleShader;
+    glm::vec3 emitterPosition;
 
-    // Renders all active particles
-    void Render();
+    ParticleSystem(int maxParticles, shaderFile::Shader& shader, glm::vec3 emitterPosition, unsigned int textureID)
+        : maxParticles(maxParticles), particleShader(shader), emitterPosition(emitterPosition), textureID(textureID) {
+        particles.reserve(maxParticles);
+        initializeBuffers();
+        resetParticles();
+        std::cout << "Current number of particles: " << particles.size() << std::endl;
+    }
 
-    // Spawns a new particle at a given position with initial velocity and color
-    void SpawnParticle(const glm::vec2& position,
-        const glm::vec2& velocity,
-        const glm::vec4& color,
-        float lifetime);
+    void resetParticles() {
+        std::cout << "Resetting particles..." << std::endl;
+        for (auto& particle : particles) {
+            resetParticle(particle);
+        }
+    }
+
+    void resetParticle(Particle& particle) {
+        particle.position = emitterPosition;
+        particle.velocity = glm::vec3(randFloat(-1.0f, 1.0f), randFloat(0.5f, 1.5f), randFloat(-1.0f, 1.0f));
+        particle.lifetime = randFloat(2.0f, 5.0f);  // Random lifetime between 2 and 5 seconds
+        particle.texCoord = glm::vec2(0.0f, 0.0f);  // Set a default texture coordinate
+        std::cout << "Particle reset: Position = " << particle.position.x << ", " << particle.position.y << ", " << particle.position.z
+            << ", Lifetime = " << particle.lifetime << std::endl;
+    }
+
+    void update(float deltaTime) {
+        for (auto& particle : particles) {
+            particle.lifetime -= deltaTime;
+            if (particle.lifetime <= 0.0f) {
+                resetParticle(particle);  // Reset particle if it has expired
+            }
+            else {
+                particle.position += particle.velocity * deltaTime;  // Update position with velocity
+            }
+        }
+    }
+
+    void EmitParticle() {
+        if (particles.size() < maxParticles) {
+            Particle newParticle;
+            newParticle.position = emitterPosition;
+            newParticle.velocity = glm::vec3(randFloat(-1.0f, 1.0f), -1.0f, 0.0f);
+            newParticle.lifetime = 10000;  // Set a fixed lifetime for the emitted particle
+            particles.push_back(newParticle);
+        }
+    }
+
+    void render(const glm::mat4& view, const glm::mat4& projection) {
+        if (particles.empty()) {
+            return;  // No particles to render
+        }
+
+        // Set up the transformations for each particle
+        std::vector<glm::mat4> particleTransforms;
+        for (const auto& particle : particles) {
+            // Set up transformation matrix for each particle
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), particle.position);
+            // You can scale or rotate the particle based on lifetime or any other factor
+            model = glm::scale(model, glm::vec3(0.1f)); // Example scale
+
+            // Store the transformation for each particle
+            particleTransforms.push_back(model);
+        }
+
+        // Bind texture for all particles
+   /*     glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textureID);*/
+
+        // Disable depth testing to ensure particles render on top of everything
+        glDisable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        particleShader.use();
+        particleShader.setMat4("view", view);
+        particleShader.setMat4("projection", projection);
+
+        // Now render each particle as a quad
+        for (size_t i = 0; i < particles.size(); ++i) {
+            // Update the model matrix for each particle
+            particleShader.setMat4("model", particleTransforms[i]);
+            particleShader.setInt("particleTexture", 0);
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);  // Render quad for each particle
+        }
+
+        // Re-enable depth testing and disable blending after rendering particles
+        glEnable(GL_DEPTH_TEST);
+        glDisable(GL_BLEND);
+    }
+
 
 private:
-    std::vector<Particle> Particles; // Container for all particles
-    unsigned int MaxParticles;       // Maximum number of particles allowed
+    void initializeBuffers() {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+        // Define the quad vertices and indices (two triangles)
+        float vertices[] = {
+            // Positions        // Texture Coords
+            -0.5f, -0.5f, 0.0f,  0.0f, 0.0f,
+            0.5f, -0.5f, 0.0f,   1.0f, 0.0f,
+            0.5f,  0.5f, 0.0f,   1.0f, 1.0f,
+            -0.5f,  0.5f, 0.0f,  0.0f, 1.0f
+        };
+
+        unsigned int indices[] = {
+            0, 1, 2,  0, 3, 2
+        };
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+    }
+
+    float randFloat(float min, float max) {
+        static std::mt19937 gen{ std::random_device{}() };
+        std::uniform_real_distribution<float> dist(min, max);
+        return dist(gen);
+    }
 };
 
-#endif PARTICLESYSTEM_H
+
+#endif // PARTICLE_SYSTEM_H
